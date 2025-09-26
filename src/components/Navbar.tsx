d73e99612f17584493491e0565d3a644bc09d428
Andreas <git@fsck.rip>
2025-09-26 15:54:27 -0400

    add navbar
    Signed-off-by: Andreas <git@fsck.rip>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
-export function Navbar(props: {userName: string}) {
-  return <nav className="navbar">
-    <p>Personal Profile Dashboard</p>
-    <p>{props.userName}</p>
-  </nav>
+export function Navbar(props: { userName: string }) {
+  return (
+    <nav className="navbar">
+      <p>Personal Profile Dashboard</p>
+      <p>{props.userName}</p>
+    </nav>
+  );
 }

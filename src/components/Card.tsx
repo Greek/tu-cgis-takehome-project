83ee26c47daf09618fec88f3c233658cabe1e2f8
Andreas <git@fsck.rip>
2025-09-26 15:52:30 -0400

    add properties to control width and centering
    Signed-off-by: Andreas <git@fsck.rip>

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,11 @@
-export function Card(props: { children: React.ReactNode }) {
-  return <div className="card">{props.children}</div>;
+export function Card(props: {
+  width?: "fixed" | "wide";
+  centered?: boolean;
+  children: React.ReactNode;
+}) {
+  return (
+    <div className={`card ${props.width ?? "wide"} ${props.centered ? "centered" : ""}`}>
+      {props.children}
+      </div>
+  );
 }

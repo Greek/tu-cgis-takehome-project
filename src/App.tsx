c8cdff7400558b91af955f103cd1a059cd547609
Andreas <git@fsck.rip>
2025-09-26 15:08:56 -0400

    format code
    Signed-off-by: Andreas <git@fsck.rip>

--- src/App.tsx
+++ src/App.tsx
@@ -33,20 +33,20 @@ function App() {
           </div>
           <div>
             <Card>
-                <h4>User Name</h4>
-                <p>{combinedName}</p>
-                <h4>Address</h4>
-                <p>{combinedAddress}</p>
-                <p>
-                  {profile.location.state}, {profile.location.country}{" "}
-                  {profile.location.postcode}
-                </p>
-                <h4>Phone Number</h4>
-                <p>{profile.phone}</p>
-                <h4>Email</h4>
-                <p>{profile.email}</p>
-                <h4>Date of Birth</h4>
-                {new Date(profile.dob.date).toLocaleDateString()}
+              <h4>User Name</h4>
+              <p>{combinedName}</p>
+              <h4>Address</h4>
+              <p>{combinedAddress}</p>
+              <p>
+                {profile.location.state}, {profile.location.country}{" "}
+                {profile.location.postcode}
+              </p>
+              <h4>Phone Number</h4>
+              <p>{profile.phone}</p>
+              <h4>Email</h4>
+              <p>{profile.email}</p>
+              <h4>Date of Birth</h4>
+              {new Date(profile.dob.date).toLocaleDateString()}
             </Card>
           </div>
         </div>
